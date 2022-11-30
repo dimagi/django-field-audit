c157daad794cb0b2c47243d57fa04d90c7066157
Graham Herceg <gherceg@dimagi.com>
2022-11-30 18:49:17 -0500

    create AuditEvent.create_audit_event()
    Pull out the overlapping logic that can be used
    once a delta is obtained by either make_audit_event,
    or the update method in the AuditingQuerySet class.

--- tests/exceptions.py
+++ tests/exceptions.py
@@ -1,2 +1,6 @@
 class MakeAuditEventException(Exception):
     """Test specific exception used when mocking make_audit_event"""
+
+
+class CreateAuditEventException(Exception):
+    """Test specific exception used when mocking create_audit_event"""
